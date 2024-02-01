c7ca8c8f641bfb22867905338d01be163db7600c
Lenon <lenonphilippi@gmail.com>
2024-01-31 23:38:06 -0300

    clean code

--- app.py
+++ app.py
@@ -1,97 +1,49 @@
 from math import ceil
 import gspread
 
-spreadsheet = gspread.service_account(filename="credentials.json")
-working_sheet = spreadsheet.open('Engenharia de Software - Desafio Lenon Fernandes Philippi')
-print(working_sheet.get_worksheet_by_id(0))
 
-# Load the workbook
-# workbook = openpyxl.load_workbook("Engenharia de Software - Desafio Lenon Fernandes Philippi.xlsx")
+def get_average(p1, p2, p3):
+    return ceil(((p1 + p2 + p3) / 3) / 10)
 
-# Select the "engenharia_de_software" sheet
-sheet = working_sheet.get_worksheet_by_id(0)
 
-# Select the values
-values = sheet.get_all_values()
-
-# Iterate values, starting in 3rd row
-for row in range(3, len(values)):
-
-    # Get value from "Faltas" column
-    absences = int(values[row][2])
-
-    # Get values from "P1", "P2", "P3" columns
-    p1, p2, p3 = map(int, values[row][3:6])
-
-    # Checks for "Reprovado por Falta" situation
-    if absences > 0.25 * 60:
-
-        """
-        Stores "Situação" value for actual row if it 
-        checks ok for "Reprovado por Falta"
-        """
-        situation = "Reprovado por Falta"
-
-        """
-        Stores "Nota para Aprovação Final" value for actual
-        row if it checks ok for "Reprovado por Falta"
-        """
-        final_grade = 0
-
-    # If there's no excessive absences, then average is analyzed
+def calculate_final_grade(average):
+    if average < 5:
+        return 0
+    elif 5 <= average < 7:
+        return 10 - average
     else:
-        # Average calculation
-        average = ceil(((p1 + p2 + p3) / 3) / 10)
+        return 0
 
-        # Checks for "Reprovado por Nota" situation
-        if average < 5:
-            """
-            Stores "Situação" value for actual row if it
-            checks ok for "Reprovado por Nota"
-            """
-            situation = "Reprovado por Nota"
-        
-            """
-            Stores "Nota para Aprovação Final" value
-            for actual row if it checks ok for
-            "Reprovado por Nota"
-            """
-            final_grade = 0
 
-        # Checks for "Exame Final" situation
-        elif 5 <= average < 7:
-            """
-            Stores "Situação" value for actual row
-            if it checks of for "Exame Final"
-            """
-            situation = "Exame Final"
+def determine_situation(absences, average):
+    if absences > 0.25 * 60:
+        return "Reprovado por Falta", 0
+    elif average < 5:
+        return "Reprovado por Nota", 0
+    elif 5 <= average < 7:
+        return "Exame Final", calculate_final_grade(average)
+    else:
+        return "Aprovado", 0
 
-            # "Nota para Aprovação Final" value calculation
-            naf = 10 - average
 
-            """
-            Stores "Nota para Aprovação Final" value for
-            actual row if it checks ok for "Exame Final"
-            """
-            final_grade = naf
+def main():
+    spreadsheet = gspread.service_account(filename="credentials.json")
+    working_sheet = spreadsheet.open('Engenharia de Software - Desafio Lenon Fernandes Philippi')
+    sheet = working_sheet.get_worksheet_by_id(0)
 
-        # Checks for "Aprovado"
-        else:
-            """
-            Stores "Situação" value for actual row
-            if it checks of for "Aprovado"
-            """
-            situation = "Aprovado"
+    values = sheet.get_all_values()
 
-            """
-            Stores "Nota para Aprovação Final" value for
-            actual row if it checks ok for "Aprovado"
-            """
-            final_grade = 0
+    for row in range(3, len(values)):
+        absences = int(values[row][2])
+        p1, p2, p3 = map(int, values[row][3:6])
 
-    """
-    Updates sheet with situation ("Situação")
-    and final grade ("Nota para Aprovação Final") values
-    """
-    sheet.update_cell(row + 1, 7, situation)
-    sheet.update_cell(row + 1, 8, final_grade)
+        average = get_average(p1, p2, p3)
+
+        situation, final_grade = determine_situation(absences, average)
+
+        sheet.update_cell(row + 1, 7, situation)
+        sheet.update_cell(row + 1, 8, final_grade)
+
+
+if __name__ == "__main__":
+    main()
